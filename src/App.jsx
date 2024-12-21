547ddea790a2a6f76257259c5526534dfa0248cb
Balraj <balrajdhakad@Balrajs-MacBook-Air.local>
2024-12-22 01:47:59 +0530

    Added source folder

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './index.css'
+import Login from './components/auth/logIn'
+import EmployeeDash from './components/dashboard/employeeDashboard'
+import Header from './components/sections/header'
+import AdminDash from './components/dashboard/adminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+
+function App() {
+
+  useEffect(() => {
+
+  },)
+
+  return (
+    <>
+      <Login />
+      <EmployeeDash/>
+      <AdminDash/>
+    </>
+  )
+}
+
+export default App
